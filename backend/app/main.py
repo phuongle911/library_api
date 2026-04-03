64094ebfe17bfc17d10441891de8dea426e8c83d
phuongle <phuongtle911@gmail.com>
2026-04-03 16:21:02 +0700

    updated_logging

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,6 +1,12 @@
-from fastapi import FastAPI
+import logging
+import sentry_sdk
+import os
+from sentry_sdk.integrations.fastapi import FastApiIntegration
+from fastapi import FastAPI, Request
 from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
 from starlette.exceptions import HTTPException as StarletteHTTPException
+
 from app.models import user
 from app.models import books
 from app.core.database import engine, Base
@@ -13,6 +19,7 @@ from app.routers.borrow import borrow_router
 from app.routers.job import job_router
 #from app.core.logging import setup_logging
 from app.core.logging_config import setup_logging
+from app.core.request_id import set_request_id, generate_request_id
 from app.middlewares.request_context import RequestContextMiddleware
 from app.core.exception_handlers import (
     http_exception_handler,
@@ -23,9 +30,17 @@ from app.core.exception_handlers import (
 from app.core.errors import AppError
 
 setup_logging()
+logger = logging.getLogger(__name__)
+sentry_sdk.init(
+    dsn=os.getenv("SENTRY_DNS"),
+    integrations=[FastApiIntegration()],
+    traces_sample_rate=1.0,
+    environment=os.getenv("APP_ENV", "development"),
+)
 
 app = FastAPI(title="My FastAPI App")
 app.add_middleware(RequestContextMiddleware)
+
 
 app.add_exception_handler(StarletteHTTPException, http_exception_handler)
 app.add_exception_handler(RequestValidationError, validation_exception_handler)
@@ -45,3 +60,24 @@ async def on_startup():
     # Create database tables
     async with engine.begin() as conn:
         await conn.run_sync(Base.metadata.create_all)
+
+
+@app.middleware("http")
+async def add_request_id_middleware(request: Request, call_next):
+    request_id = request.headers.geet("X-Request-ID") or generate_request_id()
+    set_request_id(request_id)
+
+    response = await call_next(request)
+    response.headers["X-Request-ID"] = request_id
+    return response
+
+
+@app.exception_handler(Exception)
+async def global_exception_handler(request: Request, exc: Exception):
+    logger.exception("UNHANDLED_EXCEPTION")
+
+    return JSONResponse(
+        status_code = 500,
+        content = {"error": "Internal Server Error", "message": str(exc), "request_id": get_request_id(),
+                   },
+    )

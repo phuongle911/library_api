03c355507991e22c345789bda1e3e0518683a68a
phuongle <phuongtle911@gmail.com>
2026-06-03 20:48:49 +0700

    fixing_internal_test_file

--- backend/tests/intergration/api/test_internal_books.py
+++ backend/tests/intergration/api/test_internal_books.py
@@ -15,3 +15,11 @@ async def test_get_internal_book_sucess(
     assert data["id"] == sample_book.id
     assert data["title"] == sample_book.title
     assert data["available_copies"] == sample_book.available_copies
+
+
+async def test_get_internal_book_without_token_returns_401(client, sample_book):
+    response = await client.get(
+        f"/api/v1/internal/books/{sample_book.id}"
+    )
+
+    assert response.status_code == http_status.HTTP_401_UNAUTHORIZED
